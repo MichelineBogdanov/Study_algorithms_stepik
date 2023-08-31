61d665696fb129e201f99f2f2be59aba9e145401
Mikhail Bogdanov <bogdanov28031996@gmail.com>
2023-08-31 20:55:22 +0300

    Bag

--- src/main/java/S5_dynamic_programming/lesson4_bag/Bag.java
+++ src/main/java/S5_dynamic_programming/lesson4_bag/Bag.java
@@ -1,5 +1,8 @@
 package S5_dynamic_programming.lesson4_bag;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Первая строка входа содержит целые числа 1≤W≤10^4 и 1≤n≤300 — вместимость рюкзака и число золотых слитков.
  * Следующая строка содержит n целых чисел 0≤w1,w2,...,wn≤10^5, задающих веса слитков.
@@ -11,12 +14,32 @@ package S5_dynamic_programming.lesson4_bag;
  * ---
  * Sample Output 1:
  * 9
- * */
+ */
 
 public class Bag {
 
     public static void main(String[] args) {
-
+        Bag bag = new Bag();
+        Scanner scanner = new Scanner(System.in);
+        int w = Integer.parseInt(scanner.nextLine().split(" ")[0]);
+        int[] weight = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int result = bag.knapsackWithoutRep(w, weight);
+        System.out.println(result);
     }
 
+    private int knapsackWithoutRep(int w, int[] weight) {
+        int n = weight.length;
+        int[][] d = new int[w + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= w; j++) {
+                d[j][i] = d[j][i - 1];
+                if (weight[i - 1] <= j) {
+                    d[j][i] = Math.max(d[j][i], d[j - weight[i - 1]][i - 1] + weight[i - 1]);
+                }
+            }
+        }
+        return d[w][n];
+    }
 }
