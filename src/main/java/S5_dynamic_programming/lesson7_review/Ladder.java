7ea49f33159a8f25ac1746bcf3c549932c4ce345
Mikhail Bogdanov <bogdanov28031996@gmail.com>
2023-09-02 21:01:03 +0300

    Ladder

--- src/main/java/S5_dynamic_programming/lesson7_review/Ladder.java
+++ src/main/java/S5_dynamic_programming/lesson7_review/Ladder.java
@@ -1,5 +1,8 @@
 package S5_dynamic_programming.lesson7_review;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Даны число 1≤n≤10^2 ступенек лестницы и целые числа -10^4≤a1≤,...,≤an≤10^4, которыми помечены ступеньки.
  * Найдите максимальную сумму, которую можно получить, идя по лестнице снизу вверх (от нулевой до n-й ступеньки),
@@ -30,7 +33,23 @@ package S5_dynamic_programming.lesson7_review;
 public class Ladder {
 
     public static void main(String[] args) {
+        Ladder ladder = new Ladder();
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        long[] stair = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
+        long result = ladder.getResult(n, stair);
+        System.out.println(result);
+    }
 
+    private long getResult(int n, long[] stair) {
+        long[] d = new long[n + 1];
+        d[1] = stair[0];
+        for (int i = 1; i < n; i++) {
+            d[i + 1] = Math.max(d[i - 1] + stair[i], d[i] + stair[i]);
+        }
+        return d[n];
     }
 
 }
